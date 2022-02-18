c11f610f06aea70683b5254c67543a122cf73276
yuta0402 <y.furukawa@sai.co.jp>
2022-02-18 11:21:54 +0900

    wip

---
+++ resources/js/app.ts
@@ -0,0 +1,9 @@
+require('./bootstrap');
+
+import { createApp } from 'vue'
+import ExampleComponent from './components/Sample.vue'
+
+
+const app = createApp({})
+app.component("SampleComponent",ExampleComponent)
+app.mount('#app')
