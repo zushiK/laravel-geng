8a27c794bdc63fa1de2064de67538a4d34843a4d
yuta0402 <y.furukawa@sai.co.jp>
2022-02-21 16:06:52 +0900

    test: 型エラーのコード

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -3,6 +3,11 @@ require('./bootstrap');
 import { createApp } from 'vue';
 import ExampleComponent from './components/Sample.vue';
 
-   const app = createApp({});
+const app = createApp({});
+let num = [1, 2, 3];
+num = 'fa';
+num = true;
+// console.log(num);
+
 app.component('SampleComponent', ExampleComponent);
 app.mount('#app');
