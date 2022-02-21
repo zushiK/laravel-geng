bf597758de7e2c3168099a03dd8aef14c7d79da8
yuta0402 <y.furukawa@sai.co.jp>
2022-02-21 14:39:13 +0900

    wip

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -3,6 +3,6 @@ require('./bootstrap');
 import { createApp } from 'vue';
 import ExampleComponent from './components/Sample.vue';
 
-const app = createApp({});
+   const app = createApp({});
 app.component('SampleComponent', ExampleComponent);
 app.mount('#app');
