33a34725a3bf887de00281cdb1f5729e1336ee00
yuta0402 <y.furukawa@sai.co.jp>
2022-02-21 16:11:02 +0900

    fix: testコード

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -4,10 +4,6 @@ import { createApp } from 'vue';
 import ExampleComponent from './components/Sample.vue';
 
 const app = createApp({});
-let num = [1, 2, 3];
-num = 'fa';
-num = true;
-// console.log(num);
 
 app.component('SampleComponent', ExampleComponent);
 app.mount('#app');
