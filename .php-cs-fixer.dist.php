2c1e65dd5acd1f35269451f7d01f5011d77b424d
yuta0402 <y.furukawa@sai.co.jp>
2022-02-21 14:25:33 +0900

    chore: php-cs-fixer

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+$finder = PhpCsFixer\Finder::create()
+  ->in([
+    __DIR__ . '/app',
+    __DIR__ . '/config',
+    __DIR__ . '/database',
+    __DIR__ . '/routes',
+    __DIR__ . '/tests',
+  ]);
+
+$config = new PhpCsFixer\Config();
+
+return $config
+  ->setRiskyAllowed(true)
+  ->setRules([
+    '@PSR2' => true, //phpintelephenseと同じ
+    'trim_array_spaces' => true, //func( arg ) => func(arg);
+    'class_attributes_separation' => true, //use HasApiTokens, HasFactory, Notifiable; これが縦にならないように
+  ])
+  ->setFinder($finder)
+  ->setCacheFile(__DIR__ . '/.php-cs-fixer.cache');
