ec85a1347e1fae2588d85645e83657394906a81e
yuta0402 <yuta0402@users.noreply.github.com>
2022-02-22 09:57:23 +0000

    Apply Format Change

--- app/Models/Operator.php
+++ app/Models/Operator.php
@@ -6,18 +6,18 @@ use App\Models\BaseModel;
 
 class Operator extends BaseModel
 {
-  //テーブル名指定
-  protected $table = 'dtb_operators';
+    //テーブル名指定
+    protected $table = 'dtb_operators';
 
-  // 
-  protected $primaryKey = 'operator_id';
+    //
+    protected $primaryKey = 'operator_id';
 
-  //既存のテーブルはカラムが多いので基本的には$fillableではなく$gaurdedで指定する
-  protected $guarded = ['operator_id'];
+    //既存のテーブルはカラムが多いので基本的には$fillableではなく$gaurdedで指定する
+    protected $guarded = ['operator_id'];
 
-  protected $hidden = ['password'];
+    protected $hidden = ['password'];
 
-  protected $casts = [
+    protected $casts = [
     'is_active' => 'boolean',
     'fld_code_list' => 'array'
   ];
