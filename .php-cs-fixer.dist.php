bfa72b7add016de693227c41a25b16a954e5d26e
yuta0402 <y.furukawa@sai.co.jp>
2022-02-24 10:54:44 +0900

    fix: 配列フォーマット

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -4,22 +4,21 @@ declare(strict_types=1);
 
 $finder = PhpCsFixer\Finder::create()
   ->in([
-    __DIR__ . '/app',
-    __DIR__ . '/config',
-    __DIR__ . '/database',
-    __DIR__ . '/routes',
-    __DIR__ . '/tests',
+      __DIR__ . '/app',
+      __DIR__ . '/config',
+      __DIR__ . '/database',
+      __DIR__ . '/routes',
+      __DIR__ . '/tests',
   ]);
 
 $config = new PhpCsFixer\Config();
 
 return $config
-  ->setRiskyAllowed(true)
-
   ->setRules([
-    '@PSR2' => true, //phpintelephenseと同じ
-    'trim_array_spaces' => true, //func( arg ) => func(arg);
-    'class_attributes_separation' => true, //use HasApiTokens, HasFactory, Notifiable; これが縦にならないように
+      '@PSR2' => true, //phpintelephenseと同じ
+      'trim_array_spaces' => true, //func( arg ) => func(arg);
+      'class_attributes_separation' => true, //use HasApiTokens, HasFactory, Notifiable; これが縦にならないように
+      'array_indentation' => true //配列整形できるように
   ])
   ->setFinder($finder)
   ->setCacheFile(__DIR__ . '/.php-cs-fixer.cache');
