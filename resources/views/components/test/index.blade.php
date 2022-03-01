f3ebed20899cfea76117433d4e707d72a4ade45a
yuta0402 <y.furukawa@sai.co.jp>
2022-03-01 14:43:45 +0900

    test: blade component /view/componentは自動で使える /view/layoutsはクラスベースのコンポーネントとして登録して使う

---
+++ resources/views/components/test/index.blade.php
@@ -0,0 +1,10 @@
+<div>
+
+
+    コンポーネント親
+    <div class=" bg-fuchsia-400">
+        <x-test.parts></x-test.parts>
+    </div>
+
+
+</div>
---
+++ resources/views/components/test/parts.blade.php
@@ -0,0 +1,4 @@
+<div>
+    これはパーツです。
+    https://readouble.com/laravel/8.x/ja/blade.html
+</div>
