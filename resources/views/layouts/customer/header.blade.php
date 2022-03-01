b47a7a9155c102279f62c3fb1e87fe9f9cb34b65
yuta0402 <y.furukawa@sai.co.jp>
2022-03-01 17:10:28 +0900

    wip

--- resources/views/layouts/customer/header.blade.php
+++ resources/views/layouts/customer/header.blade.php
@@ -1,7 +1,19 @@
 <header>
     <div class=" bg-gray-500">私はヘッダーです。</div>
+    @php
+        // dump(auth()->user());
+    @endphp
     <div>
-        <a class="text-4xl text-fuchsia-800" href="{{ route('customer.login') }}">ログインする</a>
-
+        @auth('customer')
+            <form method="POST" action="{{ route('customer.logout') }}">
+                @csrf
+                <x-responsive-nav-link :href="route('customer.logout')" onclick="event.preventDefault();
+                                                    this.closest('form').submit();">
+                    ログアウトする
+                </x-responsive-nav-link>
+            </form>
+        @else
+            <a class="text-4xl text-fuchsia-800" href="{{ route('customer.login') }}">ログインする</a>
+        @endauth
     </div>
 </header>
