7820e9260eab9f3eb68c3deef274e3c602e7c040
yuta0402 <y.furukawa@sai.co.jp>
2022-03-09 16:06:41 +0900

    wip

--- resources/views/layouts/operator/sidebar.blade.php
+++ resources/views/layouts/operator/sidebar.blade.php
@@ -1,5 +1,5 @@
 <aside class="">
-    <div class="w-52 h-screen bg-white rounded-md shadow">
+    <div class="w-52 bg-white rounded-md shadow">
         <div class="flex justify-center flex-col">
             <div class="text-gray-900 font-extrabold tracking-tight py-4 ml-3 border-gray-200 border-b">
                 基本情報管理
