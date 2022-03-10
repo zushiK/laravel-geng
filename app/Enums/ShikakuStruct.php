fecd9747ccad885a3ce9ec677a7f12ebd7821633
maythirihtet <maythirihtet@users.noreply.github.com>
2022-03-10 10:53:49 +0000

    Apply Format Change

--- app/Enums/ShikakuStruct.php
+++ app/Enums/ShikakuStruct.php
@@ -4,24 +4,22 @@ namespace App\Enums;
 
 enum ShikakuStruct: int
 {
+    case Default = 0;
+    case MG = 1;
+    case GL = 2;
+    case CBC = 3;
+    case BC = 4;
+    case Ippan = 5;
 
-  case Default = 0;
-  case MG = 1;
-  case GL = 2;
-  case CBC = 3;
-  case BC = 4;
-  case Ippan = 5;
-
-  public function label(): string
-  {
-      return match($this)
-      {
-        ShikakuStruct::Default => '未選択',
+    public function label(): string
+    {
+        return match ($this) {
+            ShikakuStruct::Default => '未選択',
         ShikakuStruct::MG => 'MG',
         ShikakuStruct::GL => 'GL',
         ShikakuStruct::CBC => 'CBC',
         ShikakuStruct::BC => 'BC',
         ShikakuStruct::Ippan => '一般',
-      };
-  }
+        };
+    }
 }
