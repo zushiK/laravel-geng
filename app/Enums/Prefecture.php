0ce3aa6b4c78ea3ba08c42eee1f171ab76c5bcf6
yuta0402 <yuta0402@users.noreply.github.com>
2022-03-11 06:38:39 +0000

    Apply Format Change

--- app/Enums/Prefecture.php
+++ app/Enums/Prefecture.php
@@ -4,58 +4,58 @@ namespace App\Enums;
 
 enum Prefecture: int
 {
-  case HOKKAIDO = 1;
-  case AOMORI = 2;
-  case IWATE = 3;
-  case MIYAGI = 4;
-  case AKITA = 5;
-  case YAMAGATA = 6;
-  case FUKUSHIMA = 7;
-  case IBARAKI = 8;
-  case TOCHIGI = 9;
-  case GUNMA = 10;
-  case SAITAMA = 11;
-  case CHIBA = 12;
-  case TOKYO = 13;
-  case KANAGAWA = 14;
-  case NIIGATA = 15;
-  case TOYAMA = 16;
-  case ISHIKAWA = 17;
-  case FUKUI = 18;
-  case YAMANASHI = 19;
-  case NAGANO = 20;
-  case GIFU = 21;
-  case SHIZUOKA = 22;
-  case AICHI = 23;
-  case MIE = 24;
-  case SHIGA = 25;
-  case KYOTO = 26;
-  case OSAKA = 27;
-  case HYOGO = 28;
-  case NARA = 29;
-  case WAKAYAMA = 30;
-  case TOTTORI = 31;
-  case SHIMANE = 32;
-  case OKAYAMA = 33;
-  case HIROSHIMA = 34;
-  case YAMAGUCHI = 35;
-  case TOKUSHIMA = 36;
-  case KAGAWA = 37;
-  case EHIME = 38;
-  case KOCHI = 39;
-  case FUKUOKA = 40;
-  case SAGA = 41;
-  case NAGASAKI = 42;
-  case KUMAMOTO = 43;
-  case OITA = 44;
-  case MIYAZAKI = 45;
-  case KAGOSHIMA = 46;
-  case OKINAWA = 47;
+    case HOKKAIDO = 1;
+    case AOMORI = 2;
+    case IWATE = 3;
+    case MIYAGI = 4;
+    case AKITA = 5;
+    case YAMAGATA = 6;
+    case FUKUSHIMA = 7;
+    case IBARAKI = 8;
+    case TOCHIGI = 9;
+    case GUNMA = 10;
+    case SAITAMA = 11;
+    case CHIBA = 12;
+    case TOKYO = 13;
+    case KANAGAWA = 14;
+    case NIIGATA = 15;
+    case TOYAMA = 16;
+    case ISHIKAWA = 17;
+    case FUKUI = 18;
+    case YAMANASHI = 19;
+    case NAGANO = 20;
+    case GIFU = 21;
+    case SHIZUOKA = 22;
+    case AICHI = 23;
+    case MIE = 24;
+    case SHIGA = 25;
+    case KYOTO = 26;
+    case OSAKA = 27;
+    case HYOGO = 28;
+    case NARA = 29;
+    case WAKAYAMA = 30;
+    case TOTTORI = 31;
+    case SHIMANE = 32;
+    case OKAYAMA = 33;
+    case HIROSHIMA = 34;
+    case YAMAGUCHI = 35;
+    case TOKUSHIMA = 36;
+    case KAGAWA = 37;
+    case EHIME = 38;
+    case KOCHI = 39;
+    case FUKUOKA = 40;
+    case SAGA = 41;
+    case NAGASAKI = 42;
+    case KUMAMOTO = 43;
+    case OITA = 44;
+    case MIYAZAKI = 45;
+    case KAGOSHIMA = 46;
+    case OKINAWA = 47;
 
     public function label(): string
     {
-          return match ($this) {
-              Prefecture::HOKKAIDO => "北海道",
+        return match ($this) {
+            Prefecture::HOKKAIDO => "北海道",
               Prefecture::AOMORI => "青森",
               Prefecture::IWATE => "岩手",
               Prefecture::MIYAGI => "宮城",
@@ -102,7 +102,6 @@ enum Prefecture: int
               Prefecture::MIYAZAKI => "宮崎",
               Prefecture::KAGOSHIMA => "鹿児島",
               Prefecture::OKINAWA => "沖縄"
-          };
-
-      }
+        };
+    }
 }
