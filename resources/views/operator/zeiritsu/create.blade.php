3e21b86b1a9fc072c06f780d7084d6cca4b89600
yuta0402 <y.furukawa@sai.co.jp>
2022-03-14 13:34:07 +0900

    fix: コンポーネント適用

--- resources/views/operator/zeiritsu/create.blade.php
+++ resources/views/operator/zeiritsu/create.blade.php
@@ -1,6 +1,5 @@
 <x-app-layout>
-
-    <div class="container mx-auto flex flex-col">
+    <x-operator.page-create>
         <div class="flex m-4 justify-between">
             <div class="text-gray-900 text-3xl font-extrabold tracking-tight">
                 税率追加
@@ -8,27 +7,21 @@
         </div>
         <form action="{{ route('operator.zeiritsu.store') }}" method="POST">
             @csrf
-            <div class="shadow sm:rounded-md sm:overflow-hidden">
-                <div class="px-4 py-5 bg-white space-y-6 sm:p-6">
-                    <div class="grid grid-cols-6 gap-6">
-                        <div class="col-span-3">
-                            <x-input name="tax_rate" extra='required'>
-                                消費税率
-                            </x-input>
-                            <x-input-error> {{ $errors->first('tax_rate') }}</x-input-error>
-                        </div>
-                        <div class="col-span-3">
-                            <x-input-datetime name="apply_date">適用日時
-                            </x-input-datetime>
-                            <x-input-error>{{ $errors->first('apply_date') }}</x-input-error>
-                        </div>
-                    </div>
+            <x-operator.page-create.form-inner>
+
+                <div class="col-span-3">
+                    <x-input name="tax_rate" extra='required'>
+                        消費税率
+                    </x-input>
+                    <x-input-error> {{ $errors->first('tax_rate') }}</x-input-error>
                 </div>
-                <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
-                    <x-button-submit>追加する</x-button-submit>
+                <div class="col-span-3">
+                    <x-input-datetime name="apply_date">適用日時
+                    </x-input-datetime>
+                    <x-input-error>{{ $errors->first('apply_date') }}</x-input-error>
                 </div>
-            </div>
+            </x-operator.page-create.form-inner>
         </form>
-    </div>
+    </x-operator.page-create>
 
 </x-app-layout>
